243ab37d4485a12de6ed0abf99cb0477f62798fb
Genar Trias Ortiz <gtrias@gmail.com>
2024-08-20 16:26:38 +0200

    not defining the upper

--- lib/public/repositories/base.rb
+++ lib/public/repositories/base.rb
@@ -17,7 +17,7 @@ module ResourceRegistry
 
       abstract!
 
-      Entity = type_template { { upper: T::Struct } }
+      Entity = type_template
 
       sig { returns(T.untyped) }
       def self.entity
