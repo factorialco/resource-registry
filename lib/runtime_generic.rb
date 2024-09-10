89110e60b845e7f341edcd0dd05cea618df5bfb7
Genar Trias Ortiz <gtrias@gmail.com>
2024-09-10 11:39:04 +0200

    recover runtimegeneric

--- lib/runtime_generic.rb
+++ lib/runtime_generic.rb
@@ -20,19 +20,19 @@ module RuntimeGeneric
   extend T::Sig
   include T::Generic
 
-  # def type_member(variance = :invariant, &blk)
-  #   # `T::Generic#type_member` just instantiates a `T::Type::TypeMember` instance and returns it.
-  #   # We use that when registering the type member and then later return it from this method.
-  #   #
-  #   # Dear developer, This part was adapted from tapioca so it can keep
-  #   # generating proper RBIs for this ad-hoc generics, Genar
-  #   Tapioca::TypeVariableModule.new(
-  #     T.cast(self, Module),
-  #     Tapioca::TypeVariableModule::Type::Member,
-  #     variance,
-  #     blk
-  #   ).tap do |type_variable|
-  #     Tapioca::Runtime::GenericTypeRegistry.register_type_variable(self, type_variable)
-  #   end
-  # end
+  def type_member(variance = :invariant, &blk)
+    # `T::Generic#type_member` just instantiates a `T::Type::TypeMember` instance and returns it.
+    # We use that when registering the type member and then later return it from this method.
+    #
+    # Dear developer, This part was adapted from tapioca so it can keep
+    # generating proper RBIs for this ad-hoc generics, Genar
+    Tapioca::TypeVariableModule.new(
+      T.cast(self, Module),
+      Tapioca::TypeVariableModule::Type::Member,
+      variance,
+      blk
+    ).tap do |type_variable|
+      Tapioca::Runtime::GenericTypeRegistry.register_type_variable(self, type_variable)
+    end
+  end
 end
