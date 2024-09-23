3507f9ca158c79531ee376d23c3053a3c103bfee
Genar Trias Ortiz <gtrias@gmail.com>
2024-09-23 16:23:19 +0200

    start testing

--- examples/sinatra-pet-shelter/app_spec.rb
+++ examples/sinatra-pet-shelter/app_spec.rb
@@ -4,7 +4,13 @@ require 'spec_helper'
 require_relative 'app'
 
 describe App do
-  it 'works' do
-    # ...
+  let(:app) { App.new }
+
+  context 'GET /' do
+    let(:response) { get '/members' }
+
+    it 'returns 200 OK' do
+      expect(response.status).to eq 200
+    end
   end
 end
