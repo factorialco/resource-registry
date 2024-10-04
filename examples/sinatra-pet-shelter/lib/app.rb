438c33087544b053d013b92df5276b3fe972fec2
Genar Trias Ortiz <gtrias@gmail.com>
2024-10-04 12:05:26 +0200

    remove active_support fixes #12

--- examples/sinatra-pet-shelter/lib/app.rb
+++ examples/sinatra-pet-shelter/lib/app.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/multi_route'
+require 'resource_registry'
+
 require 'sinatra/reloader' if development?
 
 # Main entrypoint of the application
 class App < Sinatra::Base
-  get '/' do
-    'Hello world!'
+  register Sinatra::MultiRoute
+
+  resources = ResourceRegistry::Registry.new.resources
+
+  resources.each do |resource|
+    route :get, "/#{resource}" do
+      content_type :json
+      { data: [] }.to_json
+    end
   end
 end
--- examples/sinatra-pet-shelter/spec/app_spec.rb
+++ examples/sinatra-pet-shelter/spec/app_spec.rb
@@ -5,8 +5,8 @@ require 'spec_helper'
 describe App do
   let(:app) { App.new }
 
-  context 'GET /' do
-    let(:response) { get '/' }
+  context 'GET /dogs' do
+    let(:response) { get '/dogs' }
 
     it 'returns 200 OK' do
       expect(response.status).to eq 200
