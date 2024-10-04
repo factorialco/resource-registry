848456c3024529bed83fc5c4e8b2d48404edef34
Genar Trias Ortiz <gtrias@gmail.com>
2024-10-04 16:02:40 +0200

    trying to use dry-inflector instead of active_support

--- examples/sinatra-pet-shelter/lib/repository.rb
+++ examples/sinatra-pet-shelter/lib/repository.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
+# typed: true
 
 class Repository
+  extend T::Sig
+
+  include ::ResourceRegistry::Repositories::Base
+  extend RuntimeGeneric
+
+  sig { returns(T.untyped) }
+  def self.entity
+    # Dirty hack to make Tapioca work with `tapioca dsl` command, our
+    # RuntimeGeneric is not compatible with it
+    return if defined?(Tapioca)
+
+    T.unsafe(const_get(:Entity)).inner_type[:fixed]
+  end
+
+  # Migrate to RR gem
+  sig(:final) { returns(String) }
+  def self.namespace
+    namespace = to_s.split('::Repositories').first
+    return T.must(namespace).sub('::', '') if namespace != to_s
+
+    T.must(to_s.split('::').first)
+  end
 end
