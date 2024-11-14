5f96d7cb26cf3bd9a79b0af3d66188c045b70ca2
Jacobo Bouzas Quiroga <jacobo.bouzas@factorial.co>
2024-11-14 15:13:36 +0100

    chore: remove debugging message

--- lib/runtime_generic.rb
+++ lib/runtime_generic.rb
@@ -45,7 +45,6 @@ module RuntimeGeneric
 
   def type_member(variance = :invariant, &blk)
     if defined?(Tapioca::TypeVariableModule)
-      puts 'Patching tapioca inference'
       # `T::Generic#type_member` just instantiates a `T::Type::TypeMember` instance and returns it.
       # We use that when registering the type member and then later return it from this method.
       return Tapioca::TypeVariableModule.new(
