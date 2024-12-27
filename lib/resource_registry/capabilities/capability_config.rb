ebf79f6dc9abb1376540138d23f85fe265636258
Antonio Tapiador <antonio.tapiador@protonmail.com>
2024-12-27 10:23:59 +0100

    Add CapabilityConfig resource methods (#31)
    Convinient methods to get the CapabilityConfig instance
    of a given resource

--- lib/resource_registry/capabilities/capability_config.rb
+++ lib/resource_registry/capabilities/capability_config.rb
@@ -14,12 +14,38 @@ module ResourceRegistry
       # Class methods interface for capability configuration
       module ClassMethods
         extend T::Sig
+        extend T::Generic
         extend T::Helpers
         abstract!
+
+        has_attached_class!
 
         # The key of the capability, this key will be used to take it from yaml configuration
         sig { abstract.returns(Symbol) }
         def key
+        end
+
+        sig { params(resource: Resource).returns(T::Boolean) }
+        def resource_capability?(resource:)
+          resource.capabilities.key?(key)
+        end
+
+        sig do
+          params(resource: Resource).returns(
+            T.nilable(T.attached_class)
+          )
+        end
+        def resource_capability(resource:)
+          return unless resource_capability?(resource:)
+
+          T.cast(resource.capabilities[key], T.attached_class)
+        end
+
+        sig do
+          params(resource: Resource).returns(T.attached_class)
+        end
+        def resource_capability!(resource:)
+          T.must(resource_capability(resource: ))
         end
       end
 
